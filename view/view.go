938c07272c61b8aca97c207491d5639b67d49a83
Andras Belicza <iczaaa@gmail.com>
2020-02-09 17:18:05 +0100

    Refactor frame drawing to View.drawFrame()

--- view/view.go
+++ view/view.go
@@ -22,72 +22,82 @@ type View struct {
 	w   *app.Window
 	th  *material.Theme
 	gtx *layout.Context
+
+	// New Game button model
+	newGameBtn *widget.Button
+	// Difficulty button model
+	diffBtn *widget.Button
+	// Lab size button model
+	labSizeBtn *widget.Button
+	// Selected difficulty index (in engine.Difficulties)
+	diffIdx int
+	// Selected lab size index (in engine.LabSizes)
+	labSizeIdx int
 }
 
 // New returns a new View.
 func New(w *app.Window) *View {
 	return &View{
-		w:   w,
-		th:  material.NewTheme(),
-		gtx: layout.NewContext((w.Queue())),
+		w:          w,
+		th:         material.NewTheme(),
+		gtx:        layout.NewContext((w.Queue())),
+		newGameBtn: new(widget.Button),
+		diffBtn:    new(widget.Button),
+		labSizeBtn: new(widget.Button),
+		diffIdx:    engine.DifficultyDefaultIdx,
+		labSizeIdx: engine.DefaultLabSizeIdx,
 	}
 }
 
 // Loop starts handing user input. This function only returns if the user closes the app.
 func (v *View) Loop() {
-	w := v.w
-	th := v.th
-	gtx := v.gtx
-
-	var (
-		newGameBtn = new(widget.Button)
-		diffBtn    = new(widget.Button)
-		labSizeBtn = new(widget.Button)
-	)
-
-	var (
-		diffIdx    = engine.DifficultyDefaultIdx
-		labSizeIdx = engine.DefaultLabSizeIdx
-	)
-
-	for e := range w.Events() {
+	for e := range v.w.Events() {
 		switch e := e.(type) {
 		case system.FrameEvent:
-			log.Println("frame")
-			gtx.Reset(e.Config, e.Size)
-
-			for diffBtn.Clicked(gtx) {
-				diffIdx = (diffIdx + 1) % len(engine.Difficulties)
-			}
-			for labSizeBtn.Clicked(gtx) {
-				labSizeIdx = (labSizeIdx + 1) % len(engine.LabSizes)
-			}
-
-			layout.N.Layout(gtx, func() {
-				layout.UniformInset(unit.Px(5)).Layout(gtx, func() {
-					layout.Flex{Axis: layout.Horizontal, Alignment: layout.Middle}.Layout(gtx,
-						layout.Rigid(func() {
-							layout.Inset{Left: unit.Px(10), Right: unit.Px(10)}.Layout(gtx, func() {
-								th.Button("New Game").Layout(gtx, newGameBtn)
-							})
-						}),
-						layout.Rigid(func() {
-							layout.Inset{Left: unit.Px(10), Right: unit.Px(10)}.Layout(gtx, func() {
-								th.Button("Difficulty: "+engine.Difficulties[diffIdx].String()).Layout(gtx, diffBtn)
-							})
-						}),
-						layout.Rigid(func() {
-							layout.Inset{Left: unit.Px(10), Right: unit.Px(10)}.Layout(gtx, func() {
-								th.Button("Lab size: "+engine.LabSizes[labSizeIdx].String()).Layout(gtx, labSizeBtn)
-							})
-						}),
-					)
-				})
-			})
-
-			e.Frame(gtx.Ops)
+			v.drawFrame(e)
 		case system.DestroyEvent:
 			log.Println("Goodbye!")
 		}
 	}
+}
+
+// drawFrame draws a frame of the window.
+func (v *View) drawFrame(e system.FrameEvent) {
+	log.Println("frame")
+
+	th := v.th
+	gtx := v.gtx
+
+	gtx.Reset(e.Config, e.Size)
+
+	for v.diffBtn.Clicked(gtx) {
+		v.diffIdx = (v.diffIdx + 1) % len(engine.Difficulties)
+	}
+	for v.labSizeBtn.Clicked(gtx) {
+		v.labSizeIdx = (v.labSizeIdx + 1) % len(engine.LabSizes)
+	}
+
+	layout.N.Layout(gtx, func() {
+		layout.UniformInset(unit.Px(5)).Layout(gtx, func() {
+			layout.Flex{Axis: layout.Horizontal, Alignment: layout.Middle}.Layout(gtx,
+				layout.Rigid(func() {
+					layout.Inset{Left: unit.Px(10), Right: unit.Px(10)}.Layout(gtx, func() {
+						th.Button("New Game").Layout(gtx, v.newGameBtn)
+					})
+				}),
+				layout.Rigid(func() {
+					layout.Inset{Left: unit.Px(10), Right: unit.Px(10)}.Layout(gtx, func() {
+						th.Button("Difficulty: "+engine.Difficulties[v.diffIdx].String()).Layout(gtx, v.diffBtn)
+					})
+				}),
+				layout.Rigid(func() {
+					layout.Inset{Left: unit.Px(10), Right: unit.Px(10)}.Layout(gtx, func() {
+						th.Button("Lab size: "+engine.LabSizes[v.labSizeIdx].String()).Layout(gtx, v.labSizeBtn)
+					})
+				}),
+			)
+		})
+	})
+
+	e.Frame(gtx.Ops)
 }
