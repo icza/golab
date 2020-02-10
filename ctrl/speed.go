41d96bc1df96394e371d3f6a6c5514675ac1a448
Andras Belicza <iczaaa@gmail.com>
2020-02-10 10:57:52 +0100

    Add speed.go

---
+++ ctrl/speed.go
@@ -0,0 +1,39 @@
+package ctrl
+
+import (
+	"fmt"
+	"time"
+)
+
+// Speed of the game.
+type Speed struct {
+	Name string
+
+	loopDelay time.Duration
+
+	Default bool
+}
+
+func (s *Speed) String() string {
+	fps := time.Second / s.loopDelay
+	return fmt.Sprintf("%s (%d FPS)", s.Name, fps)
+}
+
+// Speeds is a slice of all, ordered speeds.
+var Speeds = []*Speed{
+	&Speed{Name: "Slow", loopDelay: 67 * time.Millisecond},                  // ~15 FPS
+	&Speed{Name: "Normal", loopDelay: 50 * time.Millisecond, Default: true}, // ~20 FPS
+	&Speed{Name: "Fast", loopDelay: 40 * time.Millisecond},                  // ~25 FPS
+}
+
+// SpeedDefaultIdx is the index of the default speed in Speeds.
+var SpeedDefaultIdx int
+
+func init() {
+	for i, s := range Speeds {
+		if s.Default {
+			SpeedDefaultIdx = i
+			break
+		}
+	}
+}
